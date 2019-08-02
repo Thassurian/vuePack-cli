6e5d8aae985cb4c8e78dd4457bc3324bed9598aa
Thassurian <e.lang.yao.e@qq.com>
2019-08-02 16:57:03 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+import axios from './lib/apiService';    //封装过的axios请求，用来统一处理请求
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
-
+import notice from './lib/noticeService.js';    //封装过的弹窗消息组件
+Vue.prototype.$notice = notice.notice;
+Vue.prototype.$message = notice.message;
+Vue.prototype.setStorage = (key,obj)=>{
+	window.localStorage.setItem(key, window.JSON.stringify(obj))
+};
+Vue.prototype.getStorage = (key)=>{
+	return window.JSON.parse(window.localStorage.getItem(key))
+};
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
